6774303753ee888fa85d4da384f2b74584973de7
ssuish <quidor.adrean@gmail.com>
2026-02-26 11:33:38 +0800

    Add ParentNode class for HTML rendering and corresponding unit tests.
    TODO: watch for more edge cases in html recursion

---
+++ src/parentnode.py
@@ -0,0 +1,19 @@
+from htmlnode import HTMLNode
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag=tag, value=None, children=children, props=props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Parent node doesn't have a tag")
+
+        if self.children is None:
+            raise ValueError("Parent node doesn't have a children")
+
+        props_str = self.props_to_html()
+
+        children_html = "".join(child.to_html() for child in self.children)
+
+        return f"<{self.tag}{props_str}>{children_html}</{self.tag}>"
