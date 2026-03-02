cc5336830abd615e4b0e8337475331541f6d3a8d
ssuish <quidor.adrean@gmail.com>
2026-03-02 11:25:05 +0800

    Add BlockType enum and block_to_block_type function to classify Markdown blocks.

--- src/block.py
+++ src/block.py
@@ -1,4 +1,17 @@
-def markdown_to_blocks(markdown: str) -> list[str]:
+import re
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
+def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     result = []
     for block in blocks:
@@ -11,3 +24,25 @@ def markdown_to_blocks(markdown: str) -> list[str]:
         result.append("\n".join(lines))
 
     return result
+
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    # TODO: Do a line-by-line check for heading, quote, unordered list, and ordered list (w/ increments)
+    #       Keep the code block check implementation.
+
+    for l in lines:
+        match block:
+            case re.search(r"^#{1,6} .+", block):
+                return BlockType.HEADING
+            case block if block.startswith("```/n") and block.endswith("```"):
+                return BlockType.CODE
+            case block.startswith(">"):
+                return BlockType.QUOTE
+            case block.startswith("- "):
+                return BlockType.UNORDERED_LIST
+            case re.search(r"^(\d+)\. .+", block):
+                return BlockType.ORDERED_LIST
+            case _:
+                return BlockType.PARAGRAPH
--- src/test_block.py
+++ src/test_block.py
@@ -1,5 +1,5 @@
 import unittest
-from block import markdown_to_blocks
+from block import block_to_block_type, markdown_to_blocks
 
 
 class TestBlock(unittest.TestCase):
@@ -20,6 +20,28 @@ class TestBlock(unittest.TestCase):
                 "This is **bolded** paragraph",
                 "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
                 "- This is a list\n- with items",
+            ],
+        )
+
+    def test_block_to_BlockType(self):
+        # TODO: Do a test for every block type
+
+        md = """
+                This is **bolded** paragraph
+
+                This is another paragraph with _italic_ text and `code` here
+                This is the same paragraph on a new line
+
+                - This is a list
+                - with items
+                """
+        blocks = block_to_block_type(md)
+        self.assertEqual(
+            blocks,
+            [
+                "paragraph",
+                "paragraph",
+                "unordered list",
             ],
         )
 
