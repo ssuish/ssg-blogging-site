eae7ef56b76d276f3067c1d3bb43704008e738d0
ssuish <quidor.adrean@gmail.com>
2026-03-07 23:06:08 +0800

    Refactor title extraction and improve error handling in markdown processing

--- src/generate_page.py
+++ src/generate_page.py
@@ -3,9 +3,12 @@ from block import markdown_to_html_node
 
 
 def extract_title(md):
-    if len(md) - len(md.lstrip("#")) == 1:
-        return md.lstrip("#").strip(" ")
-    raise ValueError("There's no h1 header.")
+    lines = md.split("\n")
+    for line in lines:
+        if line.startswith("# "):
+            return line[2:]
+    raise ValueError("There's no title")
+
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
@@ -13,14 +16,14 @@ def generate_page(from_path, template_path, dest_path):
     template = None
 
     try:
-        with open(from_path, 'r') as f: 
+        with open(from_path, "r") as f:
             md = f.read()
     except FileNotFoundError:
         print(f"Missing file at {from_path}")
 
     try:
         with open(template_path, "r") as f:
-            template  = f.read()
+            template = f.read()
     except FileNotFoundError:
         print(f"Missing file at {from_path}")
 
@@ -32,7 +35,7 @@ def generate_page(from_path, template_path, dest_path):
     template = template.replace("{{ Content }}", html_str)
 
     dir_path = os.path.dirname(dest_path)
-    os.makedirs(dir_path, exist_ok=True)
+    os.makedirs(os.path.abspath(dir_path), exist_ok=True)
 
-    with open(dest_path, 'w') as f:
+    with open(dest_path, "w") as f:
         f.write(template)
