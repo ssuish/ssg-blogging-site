b81bb77e68b4723d9e9347864cdffeef9ca1fab5
ssuish <quidor.adrean@gmail.com>
2026-03-08 16:49:43 +0800

    Refactor page generation to support recursive directory traversal and basepath handling for asset links.

--- src/generate_page.py
+++ src/generate_page.py
@@ -10,7 +10,19 @@ def extract_title(md):
     raise ValueError("There's no title")
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page_recursive(from_dir, template, dest_dir, basepath):
+    for fp in os.listdir(from_dir):
+        from_path = os.path.join(from_dir, fp)
+        dest_path = os.path.join(dest_dir, fp)
+
+        if os.path.isfile(from_path):
+            new_dest = dest_path.replace(".md", ".html")
+            generate_page(from_path, template, new_dest, basepath)
+        else:
+            generate_page_recursive(from_path, template, dest_path, basepath)
+
+
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     md = None
     template = None
@@ -33,6 +45,8 @@ def generate_page(from_path, template_path, dest_path):
 
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html_str)
+    template = template.replace('href="/', f'href="/{basepath}')
+    template = template.replace('src="/', f'src="/{basepath}')
 
     dir_path = os.path.dirname(dest_path)
     os.makedirs(os.path.abspath(dir_path), exist_ok=True)
